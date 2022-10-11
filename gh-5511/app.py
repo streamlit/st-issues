26157153c23524fbe33aeb2833c33704fc817cc4
Lukas Masuch <Lukas.Masuch@gmail.com>
2022-10-11 14:06:45 +0200

    Add issue 5511

---
+++ gh-5511/app.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+params = st.experimental_get_query_params()
+st.write(params)
+
+if st.button("Clean"):
+    st.experimental_set_query_params()
+
+if st.button("Set with rerun"):
+    st.experimental_set_query_params(q="with_rerun")
+    st.experimental_rerun()
