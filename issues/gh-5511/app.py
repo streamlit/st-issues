5ba8c3b790ca89e6945241cdf417e64d783130b8
lukasmasuch <lukas.masuch@gmail.com>
2022-10-11 14:25:37 +0200

    Add as dedicated app

--- issues/gh-5511/app.py
+++ issues/gh-5511/app.py
@@ -7,5 +7,5 @@ if st.button("Clean"):
     st.experimental_set_query_params()
 
 if st.button("Set with rerun"):
-    st.experimental_set_query_params(**params, q="with_rerun")
+    st.experimental_set_query_params(q="with_rerun")
     st.experimental_rerun()
