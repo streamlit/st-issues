2a96542ea5ad2dfd1604486c30b085fba5bffc38
lukasmasuch <lukas.masuch@gmail.com>
2022-10-11 16:04:30 +0200

    Update issue

--- issues/gh-5517/app.py
+++ issues/gh-5517/app.py
@@ -3,7 +3,7 @@ import streamlit as st
 count_of_files = 60
 progress_bar = st.progress(0.0)
 counter = 0
-file_paths = ["1", "2"]
+file_paths = [str(i) for i in range(count_of_files)]
 
 for file in file_paths:
     print(counter)
