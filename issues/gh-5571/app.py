ddfd64c3910227eb114925b2ec2f7c1d62d99d21
Karen Javadyan <kajarenc@gmail.com>
2022-10-21 13:34:54 +0400

    issue 5571 demo code

--- issues/gh-5571/app.py
+++ issues/gh-5571/app.py
@@ -1,3 +1,21 @@
 import streamlit as st
+from datetime import date, timedelta
 
-st.write("TEST")
+today = date.today()
+today_minus_30 = today - timedelta(days=30)
+
+# Date input - doesn't work
+st.session_state.my_date_input = (today_minus_30, today)
+st.date_input(
+    label="Date input",
+    max_value=today,
+    key="my_date_input"
+)
+
+# Date input - works
+st.date_input(
+    label="Date input 2",
+    max_value=today,
+    value=(today_minus_30, today),
+    key="my_date_input_2"
+)
