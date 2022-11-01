a9eb3e12550ccb3dd7c80510eeb9b51dbf72b794
Lukas Masuch <Lukas.Masuch@gmail.com>
2022-11-01 13:42:13 +0100

    Update app.py

--- issues/gh-4149/app.py
+++ issues/gh-4149/app.py
@@ -1,5 +1,5 @@
 import pandas as pd
 import streamlit as st
 
-df = pd.DataFrame([[{‘1’:‘2’}, ‘abc’], [{‘6’:‘3’}, 'def]], columns= [‘a’,‘b’])
+df = pd.DataFrame([[{"1":"2"}, "abc"], [{"6":"3"}, "def"]], columns= ["a","b"])
 st.dataframe(df)
