7ff53b96be545179491a4d89db9b3da4bf3138b1
Lukas Masuch <Lukas.Masuch@gmail.com>
2023-02-01 21:35:55 +0100

    Create issue 6008

---
+++ issues/gh-6008/app.py
@@ -0,0 +1,29 @@
+import pandas as pd
+import streamlit as st
+
+df = pd.DataFrame(data= {
+    "State" : ["Taken", "Booked", "Free", "Free", "Free", "Reserved", "Reserved"],
+    "Till"  : ["2023-01-25 03:00"] + ([""]*6),
+    "By"    : ["one@some-domain.com", "(two@some-domain.com)", "", "", "", "(three@some-domain.com)", "(four@some-domain.com)"]
+})
+
+state_colors_translated = {
+    "Free"     : "LightGreen",
+    "Booked"   : "Wheat",
+    "Taken"    : "LightCoral",
+    "Reserved" : "LightGray",
+}
+
+   
+st.subheader("App name")
+st.write('**Alex**, welcome!')
+st.write("")
+
+state_by_place = {i:df["State"][i] for i in df.index}
+
+df_styled = df.style \
+                    .applymap(lambda c: f"background-color: {state_colors_translated[c]}", subset=["State"]) \
+                    .applymap(lambda c: "color: lightgray" if isinstance(c, str) and c and c[0]=="(" else "")
+
+st.write('State:')
+st.dataframe(df_styled, use_container_width=True)
