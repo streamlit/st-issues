76bcd644aae793803aa5a4af20a0db0736a9afdc
Lukas Masuch <Lukas.Masuch@gmail.com>
2023-02-03 21:56:51 +0100

    Add issue: 6060

---
+++ issues/gh-6060/app.py
@@ -0,0 +1,11 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+df = pd.DataFrame(
+   np.random.randn(10, 20),
+   columns=('col %d' % i for i in range(20)))
+
+st.dataframe(df.style.highlight_max(axis=0))
+
+st.markdown(df.style.highlight_max(axis=0).to_html(), unsafe_allow_html=True)
