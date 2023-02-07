7a57ab599f1bc17cf8c3844b122ab2172d4537b1
Lukas Masuch <Lukas.Masuch@gmail.com>
2023-02-07 15:12:19 +0100

    Update issue

--- issues/gh-6060/app.py
+++ issues/gh-6060/app.py
@@ -9,3 +9,7 @@ df = pd.DataFrame(
 st.dataframe(df.style.highlight_max(axis=0))
 
 st.markdown(df.style.highlight_max(axis=0).to_html(), unsafe_allow_html=True)
+
+st.write("---")
+
+st.dataframe(df.style.highlight_max(axis=0, props='color:darkgrey;background-color:yellow;'))
