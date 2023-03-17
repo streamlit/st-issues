e84d198d4901aafe6cd8476cbb73f3e84dce7c60
Lukas Masuch <Lukas.Masuch@gmail.com>
2023-03-17 11:37:26 +0100

    Update app.py

--- issues/gh-6269/app.py
+++ issues/gh-6269/app.py
@@ -8,4 +8,4 @@ chart = alt.Chart(pd.DataFrame(dict(x=list(range(10)),y=list(range(10))))).mark_
 )
 st.altair_chart(alt.Chart.from_json(chart.to_json()))
 st.json(chart.to_json())
-st.vega_lite_chart(chart.to_json())
+st.vega_lite_chart(json.loads(chart.to_json()))
