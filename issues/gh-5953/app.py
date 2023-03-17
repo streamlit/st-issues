4dc5d3249da96207742b73ecb00ffdb8e80ab0db
Lukas Masuch <Lukas.Masuch@gmail.com>
2023-03-17 14:53:25 +0100

    Update app.py

--- issues/gh-5953/app.py
+++ issues/gh-5953/app.py
@@ -1,16 +1,16 @@
 import streamlit as st
 import pandas as pd
 
-max_elements = pd.get_option("styler.render.max_elements")  # default: 262144
-print(max_elements)
+# max_elements = pd.get_option("styler.render.max_elements")  # default: 262144
+# print(max_elements)
 
 # big example with default styler.render.max_elements
 
-df = pd.DataFrame(list(range(max_elements + 1)))
+# df = pd.DataFrame(list(range(max_elements + 1)))
 # This next line always works
-st.dataframe(df)
+# st.dataframe(df)
 # Applying formatting fails based on dataframe size. Try commenting out for running smaller example below.
-st.dataframe(df.style.format('{:03d}'))
+# st.dataframe(df.style.format('{:03d}'))
 
 
 # small example with small custom styler.render.max_elements
