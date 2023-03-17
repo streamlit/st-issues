3912afe6daf48643b6df2436cd8a0f2798154d08
Lukas Masuch <Lukas.Masuch@gmail.com>
2023-03-17 11:50:17 +0100

    Update app.py

--- issues/gh-6324/app.py
+++ issues/gh-6324/app.py
@@ -2,8 +2,6 @@ import pandas as pd
 import streamlit as st
 import plotly.express as px
 
-st.set_page_config(layout="wide")  # Not required, but makes bug more obvious, since every resize then affects the plot
-
 df = pd.DataFrame([[1, 2, 3], [4, 5, 6]])
 
 st.plotly_chart(px.line(df),
