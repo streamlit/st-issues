7240783a4f5f821177411828a0580a9fc1a6a53d
Lukas Masuch <Lukas.Masuch@gmail.com>
2023-05-25 23:57:22 +0200

    Add issue

---
+++ issues/gh-6745/app.py
@@ -0,0 +1,11 @@
+import streamlit as st
+import pandas as pd
+
+df = pd.DataFrame(columns=['Name', 'Age', 'Amt'])
+
+edited_df = st.experimental_data_editor(df, num_rows='dynamic', key="data_editor_1")
+
+st.subheader("Fixed version:")
+
+df = pd.DataFrame(columns=['Name', 'Age', 'Amt'], index=pd.RangeIndex(start=0, step=1))
+edited_df = st.experimental_data_editor(df, num_rows='dynamic', key="data_editor_2")
