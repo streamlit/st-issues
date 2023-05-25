6d01e033a5c67128f10050221dc9fdcd6274fef4
Lukas Masuch <Lukas.Masuch@gmail.com>
2023-05-25 23:58:56 +0200

    Update issue

--- issues/gh-6745/app.py
+++ issues/gh-6745/app.py
@@ -4,6 +4,7 @@ import pandas as pd
 df = pd.DataFrame(columns=['Name', 'Age', 'Amt'])
 
 edited_df = st.experimental_data_editor(df, num_rows='dynamic', key="data_editor_1")
+st.caption("This is working fine with pandas >= 2, but not with older versions")
 
 st.subheader("Fixed version:")
 
