5bd6d31e05059c24fbc0a366186de2504a85ab54
Lukas Masuch <Lukas.Masuch@gmail.com>
2023-06-28 12:18:40 +0200

    Update app.py

--- issues/gh-6903/app.py
+++ issues/gh-6903/app.py
@@ -20,16 +20,16 @@ det, ens = getEnsemblePlotData(df)
 
 
 detChart = alt.Chart(det).mark_line(color='black').encode(
-x=alt.X("times\:T").axis(title="Time (PST)"),
-y=alt.Y("flow\:Q").axis(title="Flow (CFS)")
+x=alt.X("times:T").axis(title="Time (PST)"),
+y=alt.Y("flow:Q").axis(title="Flow (CFS)")
 )
 
 selection = alt.selection_point(fields = ["member"], bind="legend")
 domain = [str(i) for i in range(1980,2021)]
 range_ = ['grey']*41
 ensChart = alt.Chart(ens).mark_line( strokeWidth=0.5).encode(
-    x=alt.X("times\:T").axis(title="Time (PST)"),
-    y=alt.Y("flow\:Q").axis(title="Flow (CFS)"),
+    x=alt.X("times:T").axis(title="Time (PST)"),
+    y=alt.Y("flow:Q").axis(title="Flow (CFS)"),
     color = alt.Color("member").legend(columns=2, symbolLimit=41).scale(domain=domain, range = range_),
     opacity= alt.condition(selection, alt.value(1), alt.value(0.2))
 ).add_params(
