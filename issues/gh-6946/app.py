155673e13c1fc4158dd1bcb4074a638b9a21f8a3
Lukas Masuch <Lukas.Masuch@gmail.com>
2023-07-05 18:40:25 +0200

    Update app.py

--- issues/gh-6946/app.py
+++ issues/gh-6946/app.py
@@ -2,6 +2,8 @@ import streamlit as st
 
 sb_target_variable = st.selectbox("Seleccione Variable target: ", ["2112:CV201-Split.F80.mm:A ➡ CONC Split Faja CV-201 Tamaño de tamiz que deja pasar 80% de las particulas"])
 
+st.subheader("Use st.write")
 st.write(sb_target_variable)
 
-st.markdown("2112:CV201-Split.F80.mm:A ➡ CONC Split Faja CV-201 Tamaño de tamiz que deja pasar 80% de las particulas")
+st.subheader("Use st.text")
+st.text(sb_target_variable)
