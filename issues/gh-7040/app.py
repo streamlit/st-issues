3051c4533855499ee35dad36d8378abd6c407386
Lukas Masuch <Lukas.Masuch@gmail.com>
2023-07-25 00:03:30 +0200

    Add issue

---
+++ issues/gh-7040/app.py
@@ -0,0 +1,12 @@
+import streamlit as st
+import time
+
+button = st.radio("How many tabs?", [5, 6])
+tab_names = list(map(str, range(button)))
+
+
+for tab in st.tabs(tab_names):
+    with tab:
+        st.header(f"you clicked button {button}")  
+
+time.sleep(5)
