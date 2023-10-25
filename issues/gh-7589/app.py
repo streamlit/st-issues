9b12d5f6f0bf79577785cbc25fabdae05ae9c15e
Lukas Masuch <Lukas.Masuch@gmail.com>
2023-10-25 09:13:57 +0200

    Add issue

---
+++ issues/gh-7589/app.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+c1, c2 = st.columns(columns or [2, 5])
+c1.markdown('Username: :red[*]')
+
+params = {}
+params.setdefault('label_visibility', 'collapsed')
+c2.text_input('', **params)
