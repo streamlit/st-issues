12befe06e639cd3181f44d9859d92e92bbfe0733
Lukas Masuch <Lukas.Masuch@gmail.com>
2023-11-16 19:50:31 +0100

    Add issue

---
+++ issues/gh-7727/app.py
@@ -0,0 +1,16 @@
+import streamlit as st
+import pandas as pd
+
+
+df = pd.DataFrame({
+    'copy_these':['a, b, c', 'a b c', 'car, house, horse', 'car house horse'], 
+    'paste_here': ['', '', '', '']
+    })
+
+st.data_editor(df, column_config={'paste_here': st.column_config.SelectboxColumn(
+                                    'paste_here',
+                                    help='',
+                                    width='medium',
+                                    options=df['copy_these'].to_list(),
+                                    required=False
+                                ),})
