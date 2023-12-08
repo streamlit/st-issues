3ff29a782e805b0a0ca3dafa340632b712ebb188
Lukas Masuch <Lukas.Masuch@gmail.com>
2023-12-08 18:39:47 +0100

    Create app.py

---
+++ issues/gh-7816/app.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+title_writing = "Test Title"
+title_format = f'<p style="text-align: center; font-family: ' \
+               f'Arial; color: #808080; font-size: 40px; ' \
+               f'font-weight: bold;">{title_writing}</p>'
+st.markdown(title_format, unsafe_allow_html=True)
