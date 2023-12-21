f2c1684e0902ea113c727c3d1267f1ab2c5c27eb
Lukas Masuch <Lukas.Masuch@gmail.com>
2023-12-21 09:14:37 +0100

    Add issue

---
+++ issues/gh-7862/app.py
@@ -0,0 +1,24 @@
+import time
+
+import streamlit as st
+
+
+def set_state(i: int, text: str):
+    st.session_state.stage = i
+    st.session_state.lines = text.splitlines()
+
+
+time.sleep(2)
+if "stage" not in st.session_state:
+    st.session_state.stage = 0
+if st.session_state.stage == 0:
+    text = st.text_area("Input your text", height=30)
+    st.button(
+        "Begin",
+        on_click=set_state,
+        args=[1, text],
+    )
+if st.session_state.stage == 1:
+    st.title("Your text")
+    for i, line in enumerate(st.session_state.lines):
+        st.write(f"{i}. {line}")
