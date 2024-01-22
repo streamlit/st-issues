79cebad2347b78fc3cbe8ea3464f3a589a9192e7
Lukas Masuch <Lukas.Masuch@gmail.com>
2024-01-22 12:48:48 +0100

    Add issue

---
+++ issues/gh-4877/app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+if 'ctr' not in st.session_state:
+    st.session_state['ctr'] = 0
+
+def uploader_callback():
+    st.session_state['ctr'] += 1
+    print('Uploaded file #%d' % st.session_state['ctr'])
+
+st.file_uploader(label="File uploader", on_change=uploader_callback, key="file_uploader")
