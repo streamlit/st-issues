f88ff0b88f312926cefd9c60b398ca9a7a3a5a03
Lukas Masuch <Lukas.Masuch@gmail.com>
2024-01-22 13:11:47 +0100

    Add issye

---
+++ issues/gh-7822/app.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+with st.form("Form"): 
+    st.text_input("Enter text:")
+    submit = st.form_submit_button("Submit", disabled=True)
+
+if submit:  
+    "## 😵 Disabled form was submitted ?"
