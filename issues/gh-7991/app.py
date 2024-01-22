edcd46ecd2fe9e978ee01085cdc4dfa98a82d1f5
Lukas Masuch <Lukas.Masuch@gmail.com>
2024-01-22 11:55:07 +0100

    Add issue

---
+++ issues/gh-7991/app.py
@@ -0,0 +1,34 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+df = pd.DataFrame(np.random.rand(20).reshape(5, 4), columns=[f"col_{i}" for i in range(1, 5)])
+
+
+st.write("directly display dataframe w/o subset")
+
+with st.echo():
+    st.data_editor(df, key="table1", hide_index=True)
+    st.success("`hide_index` works well!")
+
+st.divider()
+
+st.write("subset (`df.loc`) dataframe prior to display")
+
+with st.echo():
+    subset_df = df.loc[df.col_1 > 0.0]
+    st.data_editor(subset_df, key="table2", hide_index=True)
+    st.error("`hide_index` doesn't work!")
+
+with st.echo():
+    st.write("possible workaround")
+    subset_df.reset_index(drop=True, inplace=True)
+    st.data_editor(subset_df, key="table3", hide_index=True)
+
+st.divider()
+
+st.write("subset data using `df.iloc` is ok")
+
+with st.echo():
+    st.data_editor(df.iloc[:2], key="table4", hide_index=True)
+    st.success("`hide_index` works, it may because `iloc` creates view instead of copy.")
