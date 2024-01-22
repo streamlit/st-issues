4216f88828b58d33476cdbbe4d86f9c07f57538c
Lukas Masuch <Lukas.Masuch@gmail.com>
2024-01-22 12:20:01 +0100

    Add issue

---
+++ issues/gh-7978/app.py
@@ -0,0 +1,4 @@
+import streamlit as st
+
+message = "**Title: Leveraging Large-Scale Language Models for Enhancing Communication Technologies**\n\nI. Introduction\nA. Overview of the evolution of language models in artificial intelligence (AI)"
+st.chat_message("assistant").write(message)
