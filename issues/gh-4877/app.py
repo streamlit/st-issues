b6dc85720a2d8f42108d77627850aa3fb8e8ef62
Lukas Masuch <Lukas.Masuch@gmail.com>
2024-01-22 12:50:47 +0100

    Update app.py

--- issues/gh-4877/app.py
+++ issues/gh-4877/app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 
+
 if 'ctr' not in st.session_state:
     st.session_state['ctr'] = 0
 
@@ -7,4 +8,5 @@ def uploader_callback():
     st.session_state['ctr'] += 1
     print('Uploaded file #%d' % st.session_state['ctr'])
 
+st.write("Callback calls:", st.session_state['ctr'])
 st.file_uploader(label="File uploader", on_change=uploader_callback, key="file_uploader")
