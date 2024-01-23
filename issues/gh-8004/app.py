ab28352b18805364ba75804b1e4a3fbe2afa59d5
Lukas Masuch <Lukas.Masuch@gmail.com>
2024-01-23 18:07:53 +0100

    Update app.py

--- issues/gh-8004/app.py
+++ issues/gh-8004/app.py
@@ -8,5 +8,8 @@ df = pd.DataFrame.from_dict(d)
 with st.expander("map"):
     st.map(df, size=2)
 
+with st.expander("map (use_container_width)"):
+    st.map(df, size=2, use_container_width=True)
+
 if st.checkbox("Add extra map"):
     st.map(df, size=2)
