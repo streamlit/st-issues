b48a857b4f4b24cb1fa36c383da280c83b56b9ec
Lukas Masuch <Lukas.Masuch@gmail.com>
2024-02-01 09:56:14 +0100

    Update app.py

--- issues/gh-8053/app.py
+++ issues/gh-8053/app.py
@@ -2,9 +2,9 @@ import streamlit as st
 import pandas as pd
 
 df = pd.DataFrame([["/test?param=KO", "/test?param=OK&demo=1"]], columns=["col1", "col2"])
-            st.data_editor(
-                data=df,
-                column_config={
-                    "col1": st.column_config.LinkColumn(display_text="/test\?param=(.*?)"),
-                    "col2": st.column_config.LinkColumn(display_text="/test\?param=(.*?)&demo=1")
-                })
+st.data_editor(
+    data=df,
+    column_config={
+       "col1": st.column_config.LinkColumn(display_text="/test\?param=(.*?)"),
+        "col2": st.column_config.LinkColumn(display_text="/test\?param=(.*?)&demo=1")
+})
