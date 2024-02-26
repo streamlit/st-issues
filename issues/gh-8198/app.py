7afbb08af6a0c28321cf46bf026a5f05034b3ae1
Lukas Masuch <Lukas.Masuch@gmail.com>
2024-02-26 18:26:03 +0100

    Add issue

---
+++ issues/gh-8198/app.py
@@ -0,0 +1,15 @@
+import streamlit as st
+
+# it doesn't work
+placeholder = st.empty()
+user_input = placeholder.chat_input(placeholder="your first question")
+if user_input:
+    st.write(f"text input: {user_input}")
+    user_input = placeholder.chat_input(placeholder="your second question")
+
+# it works
+placeholder = st.sidebar.empty()
+user_input = placeholder.chat_input(placeholder="your first question in sidebar")
+if user_input:
+    st.write(f"text input: {user_input}")
+    user_input = placeholder.chat_input(placeholder="your second question in sidebar")
