d1ed4564a10d15295cd3a0365e59b8536bb51804
Lukas Masuch <Lukas.Masuch@gmail.com>
2024-02-29 16:18:47 +0100

    Update app.py

--- issues/gh-8220/app.py
+++ issues/gh-8220/app.py
@@ -1,12 +1,13 @@
 import streamlit as st
 import pandas as pd
 
-df = pd.DataFrame(data={'date':[],'value':[]})
+df = pd.DataFrame(columns=['date','value'])
+
 editable_df = st.data_editor(
     data=df,
     column_config={
         "date": st.column_config.DateColumn(required=True),
-        "value": st.column_config.NumberColumn(default="float"),
+        "value": st.column_config.NumberColumn(),
     },
     num_rows="dynamic",
     key="clipboard_df",
