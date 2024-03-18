2c55b6b955ee86dd332c1a58f0ac45e8ab91cb80
Lukas Masuch <Lukas.Masuch@gmail.com>
2024-03-18 19:39:48 +0100

    Add issue

---
+++ issues/gh-8322/app.py
@@ -0,0 +1,14 @@
+import streamlit as st 
+
+st.set_page_config(layout='wide')
+help_message = '''This is a long help message, it's content is not important to I will just add some Moby Dick
+Call me Ishmael. Some years ago- never mind how long precisely- having little or no money in my purse, and nothing particular to interest me on shore, I thought I would sail about a little and see the watery part of the world. It is a way I have of driving off the spleen and regulating the circulation. Whenever I find myself growing grim about the mouth; whenever it is a damp, drizzly November in my soul; whenever I find myself involuntarily pausing before coffin warehouses, and bringing up the rear of every funeral I meet; and especially whenever my hypos get such an upper hand of me, that it requires a strong moral principle to prevent me from deliberately stepping into the street, and methodically knocking people’s hats off- then, I account it high time to get to sea as soon as I can. This is my substitute for pistol and ball. With a philosophical flourish Cato throws himself upon his sword; I quietly take to the ship. There is nothing surprising in this. If they but knew it, almost all men in their degree, some time or other, cherish very nearly the same feelings towards the ocean with me.
+'''
+
+*cols, last_col = st.columns(10)
+with last_col:
+    st.toggle('Some toggle', help=help_message)
+    
+with last_col:
+    with st.popover('Config'):
+        st.toggle('Some other toggle', help=help_message)
