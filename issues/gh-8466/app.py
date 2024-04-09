04cb7d200bcb61e178e553b20bae29041e667a37
Lukas Masuch <Lukas.Masuch@gmail.com>
2024-04-09 23:35:31 +0200

    Update app.py

--- issues/gh-8466/app.py
+++ issues/gh-8466/app.py
@@ -1,27 +1,31 @@
 import streamlit as st
 
 st.multiselect(
-            "Select 1 - works",
-            options=["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"],
-            default=["d", "e"],
-        )
-        st.multiselect(
-            "Select 2 - works",
-            options={"a", "b", "c", "d", "e", "f", "g", "h", "i", "j"},
-            default={"d", "e"},
-        )
-        st.multiselect(
-            "Select 3 - works",
-            options=[],
-            default=[],
-        )
-        st.multiselect(
-            "Select 4- works ",
-            options=list(),
-            default=list(),
-        )
-        st.multiselect(
-            "Select 5 - crash ", # with: StreamlitAPIException: Every Multiselect default value must exist in options
-            options=set(),
-            default=set(),
-        )
+"Select 1 - works",
+options=["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"],
+default=["d", "e"],
+)
+
+st.multiselect(
+"Select 2 - works",
+options={"a", "b", "c", "d", "e", "f", "g", "h", "i", "j"},
+default={"d", "e"},
+)
+
+st.multiselect(
+"Select 3 - works",
+options=[],
+default=[],
+)
+
+st.multiselect(
+"Select 4- works ",
+options=list(),
+default=list(),
+)
+
+st.multiselect(
+"Select 5 - crash ", # with: StreamlitAPIException: Every Multiselect default value must exist in options
+options=set(),
+default=set(),
+)
