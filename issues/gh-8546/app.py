ef551073cd0467dbb4cd2411f2538886700c5b95
Lukas Masuch <Lukas.Masuch@gmail.com>
2024-04-20 12:24:51 +0200

    Add isue

---
+++ issues/gh-8546/app.py
@@ -0,0 +1,32 @@
+import streamlit as st
+
+
+"# Unicode str"
+
+error_message = "'bla' does not match '^[0-9]{8}_[A-Z]+[0-9]+_[0-9]+_[A-Z0-9]+$'"
+
+st.code(type(error_message))
+
+"## OK"
+st.code(error_message)
+
+"## Bug: some '_' are missing!!!"
+st.write(error_message)
+st.warning(error_message)
+st.error(error_message)
+
+
+
+"# Byte str"
+
+error_message = b"'bla' does not match '^[0-9]{8}_[A-Z]+[0-9]+_[0-9]+_[A-Z0-9]+$'"
+
+st.code(type(error_message))
+
+"## OK"
+st.code(error_message)
+st.write(error_message)
+
+"## Bug: some '_' are missing!!!"
+st.warning(error_message)
+st.error(error_message)
