3706833659007f99f24436931e37dfbde2887a07
Benjamin Räthlein <benjamin.raethlein@gmail.com>
2024-05-21 13:46:18 +0200

    Update gh-8725

--- issues/gh-8725/app.py
+++ issues/gh-8725/app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 def ask_ai():
-  print("")
+  st.write(st.session_state)
 
 st.text_area(
     label="Ask the AI a Question (or ask for a Code Refactor to be done): ",
@@ -10,3 +10,13 @@ st.text_area(
 col1, col2 = st.columns(2)
 with col1:
     st.button("Ask AI", on_click=ask_ai)
+
+st.header("In form")
+with st.form("agent_form"):
+      st.text_area(
+          label="Ask the AI a Question (or ask for a Code Refactor to be done): ",
+          key="p_agent_user_input",
+      )
+      col1, col2 = st.columns(2)
+      with col1:
+          st.form_submit_button("Ask AI", on_click=ask_ai)
