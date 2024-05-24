1f5d7aba890e473ccb6b3a7cf43f3e37da19b4b4
Lukas Masuch <Lukas.Masuch@gmail.com>
2024-05-25 00:43:46 +0200

    Update issue

--- issues/gh-8768/app.py
+++ issues/gh-8768/app.py
@@ -2,11 +2,15 @@ import streamlit as st
 import plotly.express as px
 import pandas as pd
 
-@st.cache_data(ttl='5h', show_spinner=False)
+
+@st.cache_data(ttl="5h", show_spinner=False)
 def linechart(plot_df: pd.DataFrame, x_label: str, y_label: str):
     fig = px.line(plot_df)
-    fig.update_layout(xaxis_title=x_label,
-                      yaxis_title=y_label)
+    fig.update_layout(xaxis_title=x_label, yaxis_title=y_label)
     st.plotly_chart(fig, use_container_width=True)
 
-linechart(plot_df: pd.DataFrame, x_label: str, y_label: str)
+
+plot_df = pd.DataFrame({"x": [1, 2, 3, 4], "y": [10, 15, 13, 17]})
+
+
+linechart(plot_df, "x", "y")
