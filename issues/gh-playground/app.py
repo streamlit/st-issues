37c82638d933bdb74b2cca1489842ff4685ea65d
Lukas Masuch <Lukas.Masuch@gmail.com>
2024-05-29 18:14:08 +0200

    Update app

--- issues/gh-playground/app.py
+++ issues/gh-playground/app.py
@@ -2,26 +2,8 @@ import streamlit as st
 import time
 import os
 
-@st.cache_data
-def cache_something():
-    time.sleep(10)
-    return 42
+if "foo" not in st.session_state:
+    st.session_state["foo"] = "bar"
+    st.rerun()
 
-
-@st.cache_resource
-def cache_something_else():
-    time.sleep(10)
-    return 42
-
-
-if st.button("Run"):
-    cache_something()
-
-if st.button("Run Other"):
-    cache_something_else()
-
-
-st.write("foo")
-
-if st.button("Trigger Exception"):
-    raise Exception("This is an exception")
+st.write("works")
