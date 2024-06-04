ac18f1b88a66c101d551798f9752d9e811215810
Lukas Masuch <Lukas.Masuch@gmail.com>
2024-06-04 16:15:04 +0200

    Update app

--- issues/gh-playground/app.py
+++ issues/gh-playground/app.py
@@ -1,9 +1,4 @@
 import streamlit as st
-import time
-import os
 
-if "foo" not in st.session_state:
-    st.session_state["foo"] = "bar"
-    st.rerun()
-
-st.write("works")
+if st.button("Raise Exception")
+    raise Exception("An uncaught exception")
