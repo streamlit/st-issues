baecd553f668b470674bd6d634b4360ca0fc9760
Lukas Masuch <Lukas.Masuch@gmail.com>
2024-06-10 18:11:39 +0200

    Update issue\

--- issues/gh-8863/app.py
+++ issues/gh-8863/app.py
@@ -1,8 +1,5 @@
 import streamlit as st 
 
-with st.echo():
-    st.markdown(":gray[This is st.markdown gray]")
-with st.echo():
-    st.caption("This is st.caption gray")
-with st.echo():
-    st.caption("**This is st.caption bold gray**")
+st.markdown(":gray[This is st.markdown gray]")
+st.caption("This is st.caption gray")
+st.caption("**This is st.caption bold gray**")
