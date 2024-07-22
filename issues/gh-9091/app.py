5d42671184cb4b8d6aa09d35fd74beb782e6a296
Lukas Masuch <Lukas.Masuch@gmail.com>
2024-07-22 12:59:17 +0200

    Add issue

---
+++ issues/gh-9091/app.py
@@ -0,0 +1,19 @@
+import streamlit as st
+import altair as alt
+from vega_datasets import data
+
+source = data.cars()
+
+chart = alt.Chart(source, width=100, height=100).mark_point().encode(
+    x="Horsepower:Q",
+    y="Miles_per_Gallon:Q",
+)
+
+with st.echo():
+    st.altair_chart(chart.facet(column="Origin:N"), use_container_width=True)
+with st.echo():
+    st.altair_chart(chart.encode(column="Origin:N"), use_container_width=True)
+with st.echo():
+    st.altair_chart(chart.facet(row="Origin:N"), use_container_width=True)
+with st.echo():
+    st.altair_chart(chart.encode(row="Origin:N"), use_container_width=True)
