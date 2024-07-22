43360ae18c3414216d5aac865a5460f6671c476e
Lukas Masuch <Lukas.Masuch@gmail.com>
2024-07-22 12:51:48 +0200

    Update app.py

--- issues/gh-9112/app.py
+++ issues/gh-9112/app.py
@@ -3,6 +3,7 @@ import streamlit as st
 @st.experimental_dialog(title="Example", width="large")
 def show_dialog():
     st.markdown("\n[1,2,3]\n")
+    st.code("\n[1,2,3]\n")
     st.json([1, 2, 3])
 
 if st.button("Open"):
