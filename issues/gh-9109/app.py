a089d8db86076139a4c10bfc5d18baf6eccf2bbb
Lukas Masuch <Lukas.Masuch@gmail.com>
2024-07-22 13:38:50 +0200

    Add issue

---
+++ issues/gh-9109/app.py
@@ -0,0 +1,28 @@
+x = 42
+
+for _ in []:
+    pass
+else:
+    "x", x
+
+while False:
+    pass
+else:
+    "x", x
+
+try:
+    pass
+except NameError:
+    pass
+else:
+    "x", x
+
+try:
+    raise ExceptionGroup("exception group", [ValueError()])
+except* ValueError:
+    "x", x
+
+
+match x:
+    case _:
+        "x", x
