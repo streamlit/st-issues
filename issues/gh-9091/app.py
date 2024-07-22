f6693746ffc205cf3e744e38078b699f7c7c94ae
Lukas Masuch <Lukas.Masuch@gmail.com>
2024-07-22 13:01:23 +0200

    Update app.py

--- issues/gh-9091/app.py
+++ issues/gh-9091/app.py
@@ -10,12 +10,6 @@ chart = alt.Chart(source, width=100, height=100).mark_point().encode(
 )
 
 st.altair_chart(chart.facet(column="Origin:N"), use_container_width=True)
-
-with st.echo():
-    st.altair_chart(chart.facet(column="Origin:N"), use_container_width=True)
-with st.echo():
-    st.altair_chart(chart.encode(column="Origin:N"), use_container_width=True)
-with st.echo():
-    st.altair_chart(chart.facet(row="Origin:N"), use_container_width=True)
-with st.echo():
-    st.altair_chart(chart.encode(row="Origin:N"), use_container_width=True)
+st.altair_chart(chart.encode(column="Origin:N"), use_container_width=True)
+st.altair_chart(chart.facet(row="Origin:N"), use_container_width=True)
+st.altair_chart(chart.encode(row="Origin:N"), use_container_width=True)
