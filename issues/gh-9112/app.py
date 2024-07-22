f34fde3fe5ddda108e686a897fe30daefaff2eaf
Lukas Masuch <Lukas.Masuch@gmail.com>
2024-07-22 12:50:29 +0200

    Add issue

---
+++ issues/gh-9112/app.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+@st.experimental_dialog(title="Example", width="large")
+def show_dialog():
+    st.markdown("\n[1,2,3]\n")
+    st.json([1, 2, 3])
+
+if st.button("Open"):
+    show_dialog()
