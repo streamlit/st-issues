2fcb7080ad53507f8c1ee62a654123c8ecde1a31
Lukas Masuch <Lukas.Masuch@gmail.com>
2024-07-29 18:37:03 +0200

    Update app.py

--- issues/gh-9141/app.py
+++ issues/gh-9141/app.py
@@ -4,16 +4,16 @@ with st.expander("# H1 Heading"):
     st.write("Hello world")
 
 with st.expander("## H2 Heading"):
-    st.write(""Hello world")
+    st.write("Hello world")
 
 with st.expander("### H3 Heading"):
-    st.write(""Hello world")
+    st.write("Hello world")
 
 with st.expander("#### H4 Heading"):
-    st.write(""Hello world")
+    st.write("Hello world")
 
 with st.expander("##### H5 Heading"):
-    st.write(""Hello world")
+    st.write("Hello world")
 
 with st.expander("###### H6 Heading"):
-    st.write(""Hello world")
+    st.write("Hello world")
