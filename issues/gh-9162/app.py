ac49f069ac46427aa04cfa30aa28be065057f3f9
Lukas Masuch <Lukas.Masuch@gmail.com>
2024-07-29 18:39:04 +0200

    Add issue

---
+++ issues/gh-9162/app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+
+st.set_page_config(layout="wide")
+st.write(f'Streamlit {st.__version__}')
+df = pd.DataFrame({'x': [1, 2, 3], 'y': [10, 18, 13]})
+g = px.bar(df, x='x', y='y')
+st.plotly_chart(g, use_container_width=False)
+st.plotly_chart(g, use_container_width=True)
