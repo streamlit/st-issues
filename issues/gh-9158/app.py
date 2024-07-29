f7eb9e84ee6bbbcbfe3f165b99fa84ba2e6ec521
Lukas Masuch <Lukas.Masuch@gmail.com>
2024-07-29 20:44:04 +0200

    Update app.py

--- issues/gh-9158/app.py
+++ issues/gh-9158/app.py
@@ -9,9 +9,13 @@ else:
     st.tabs(["1", "2"])
 
 
-checkmark = st.checkbox("Yes or No - Inside Fragment")
+@st.fragment
+def show_page():
+    checkmark = st.checkbox("Yes or No - Inside Fragment")
+    
+    if checkmark:
+        st.tabs(["A", "B", "C"])
+    else:
+        st.tabs(["1", "2"])
 
-if checkmark:
-    st.tabs(["A", "B", "C"])
-else:
-    st.tabs(["1", "2"])
+show_page()
