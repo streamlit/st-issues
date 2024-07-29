9d8ccbec2df7aa174fa894b6341488bd00c8e4e4
Lukas Masuch <Lukas.Masuch@gmail.com>
2024-07-29 20:40:07 +0200

    Add issue

--- issues/gh-9158/app.py
+++ issues/gh-9158/app.py
@@ -1,14 +1,9 @@
 import streamlit as st
 
 
-@st.fragment
-def show_page():
-    checkmark = st.checkbox("Yes or No")
+checkmark = st.checkbox("Yes or No")
 
-    if checkmark:
-        st.tabs(["A", "B", "C"])
-    else:
-        st.tabs(["1", "2"])
-
-
-show_page()
+if checkmark:
+    st.tabs(["A", "B", "C"])
+else:
+    st.tabs(["1", "2"])
