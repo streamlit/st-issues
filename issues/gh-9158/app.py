78c1b9da1bf252feb9115cc1454d9b948884ee81
Lukas Masuch <Lukas.Masuch@gmail.com>
2024-07-29 20:40:41 +0200

    Add issue

--- issues/gh-9158/app.py
+++ issues/gh-9158/app.py
@@ -1,7 +1,15 @@
 import streamlit as st
 
 
-checkmark = st.checkbox("Yes or No")
+checkmark = st.checkbox("Yes or No - Outside Fragment")
+
+if checkmark:
+    st.tabs(["A", "B", "C"])
+else:
+    st.tabs(["1", "2"])
+
+
+checkmark = st.checkbox("Yes or No - Inside Fragment")
 
 if checkmark:
     st.tabs(["A", "B", "C"])
