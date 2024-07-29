4a825a85febeb3a43140fbe18bfe564032c7d130
Lukas Masuch <Lukas.Masuch@gmail.com>
2024-07-29 18:35:52 +0200

    Add issue

---
+++ issues/gh-9141/app.py
@@ -0,0 +1,19 @@
+import streamlit as st
+
+with st.expander("# H1 Heading"):
+    "Hello world"
+
+with st.expander("## H2 Heading"):
+    "Hello world"
+
+with st.expander("### H3 Heading"):
+    "Hello world"
+
+with st.expander("#### H4 Heading"):
+    "Hello world"
+
+with st.expander("##### H5 Heading"):
+    "Hello world"
+
+with st.expander("###### H6 Heading"):
+    "Hello world"
