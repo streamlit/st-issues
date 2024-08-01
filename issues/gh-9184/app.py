bd18b5339b60a599e1bf4bb6fc75bf401267dc69
Lukas Masuch <Lukas.Masuch@gmail.com>
2024-08-01 13:52:49 +0200

    Update issue

--- issues/gh-9184/app.py
+++ issues/gh-9184/app.py
@@ -1,3 +1,5 @@
 import streamlit as st
 
-st.slider("Slider", min_value=1.50, max_value=100.50, value=(2.5, 41.5), step=1.0)
+st.slider("Slider (float step)", min_value=1.50, max_value=100.50, value=(2.5, 41.5), step=1.0)
+
+st.slider("Slider (int step)", min_value=1.50, max_value=100.50, value=(2.5, 41.5), step=1)
