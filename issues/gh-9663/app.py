7df7acfb977158ee608b9d820704387d7363e06c
Lukas Masuch <Lukas.Masuch@gmail.com>
2024-10-17 00:41:14 +0200

    Update app.py

--- issues/gh-9663/app.py
+++ issues/gh-9663/app.py
@@ -2,6 +2,7 @@ import streamlit as st
 import time
 
 original_container = st.empty()
+remaining_time_container = st.empty()
 button_state = original_container.button("send code", key='get_code_button')
 countdown_triggered = False
 if button_state:
@@ -9,7 +10,6 @@ if button_state:
         countdown_triggered = True
         original_container.empty()
         disabled_button = True
-        remaining_time_container = st.empty()
         remaining_time = 5
         while remaining_time > 0:
             temp_button = remaining_time_container.button(f"Re-send({remaining_time})", disabled=disabled_button, key=f'resend_button_{remaining_time}')
