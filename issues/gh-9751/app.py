252a27983d50d399b2916d3f2cfd525acbb04a72
Lukas Masuch <Lukas.Masuch@gmail.com>
2024-10-30 23:54:58 +0100

    Improve

--- issues/gh-9751/app.py
+++ issues/gh-9751/app.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import streamlit as st
 
-st.dataframe(pd.DataFrame([[pd.Timedelta(seconds=1), pd.Timestamp(0) + pd.Timedelta(seconds=1)]], columns=['A', 'B']),
-             column_config={c: st.column_config.TimeColumn(c) for c in ['A', 'B']})
+df = pd.DataFrame([[pd.Timedelta(seconds=1), pd.Timestamp(0) + pd.Timedelta(seconds=1)]], columns=['A', 'B'])
+st.dataframe(df, column_config={c: st.column_config.TimeColumn(c) for c in ['A', 'B']})
+st.markdown(df.to_markdown())
