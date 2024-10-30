93abf27b2619f0778117e56f839c41995601c678
Lukas Masuch <Lukas.Masuch@gmail.com>
2024-10-31 00:36:43 +0100

    Update app.py

--- issues/gh-9751/app.py
+++ issues/gh-9751/app.py
@@ -5,3 +5,4 @@ df = pd.DataFrame([[pd.Timedelta(seconds=1), pd.Timestamp(0) + pd.Timedelta(seco
 st.dataframe(df, column_config={c: st.column_config.TimeColumn(c) for c in ['A', 'B']})
 st.markdown(str(df))
 st.dataframe(df.dtypes)
+st.dataframe(df)
