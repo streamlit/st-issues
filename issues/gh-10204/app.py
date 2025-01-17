d6a43ca2828d53fbfa521c8e4837a1dd81c98aae
Lukas Masuch <Lukas.Masuch@gmail.com>
2025-01-17 23:40:33 +0100

    Add issue

---
+++ issues/gh-10204/app.py
@@ -0,0 +1,25 @@
+import streamlit as st
+
+st.selectbox(
+    "Select an option",
+    [
+        "Option 1",
+        "Option 2",
+        "Option 3",
+        "Option 4",
+        "Option 5",
+        "Option 6",
+        "Option 1",
+        "Option 2",
+        "Option 3",
+        "Option 4",
+        "Option 5",
+        "Option 6",
+        "Option 1",
+        "Option 2",
+        "Option 3",
+        "Option 4",
+        "Option 5",
+        "Option 6",
+    ],
+)
