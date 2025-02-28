3e17a8c31f8b66d0c40394579ba4c5e487736400
Lukas Masuch <Lukas.Masuch@gmail.com>
2025-02-28 15:25:31 +0100

    Add html

--- issues/gh-7007/app.py
+++ issues/gh-7007/app.py
@@ -12,6 +12,7 @@ with cols[0]:
     st.write("original table")
     st.dataframe(df, hide_index=True)
     st.table(df)
+    st.html(df.to_html())
     
 with cols[1]:
     def bg_color(row):
@@ -21,9 +22,11 @@ with cols[1]:
     styled_df = df.style.apply(bg_color, axis=1)
     st.dataframe(styled_df, hide_index=True)
     st.table(styled_df)
+    st.html(styled_df.to_html())
 
 with cols[2]:
     styled_df = styled_df.hide(["B"], axis="columns")    # obsolete: styled_df = styled_df.hide_columns(["B"])
     st.write("after hiding column B")
     st.dataframe(styled_df, hide_index=True)
     st.table(styled_df)
+    st.html(styled_df.to_html())
