61fe36dbb40c1cf4397556d3204416d8bb19ca69
Lukas Masuch <Lukas.Masuch@gmail.com>
2025-03-04 18:31:23 +0100

    Add issue

---
+++ issues/gh-10637/app.py
@@ -0,0 +1,33 @@
+import enum
+import pandas as pd
+import streamlit as st
+
+class Status(str, enum.Enum):
+    success = "Success status"
+    running = "Running status"
+    error = "Error status"
+
+
+df = pd.DataFrame(
+    {"pipeline": ["Success", "Error", "Running"], "status": [Status.success, Status.error, Status.running]}
+)
+
+
+def status_highlight(value: Status):
+    color = ""
+    match value:
+        case Status.error:
+            color = "red"
+        case Status.running:
+            color = "blue"
+        case Status.success:
+            color = "green"
+        case _:
+            color = "gray"
+
+    return "color: %s" % color
+
+
+df = df.style.map(status_highlight, subset=["status"])
+
+st.dataframe(df, column_config={"status": st.column_config.TextColumn("Status column")}, hide_index=True)
