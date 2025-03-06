e55475a2e55199878e5abf8da9526015a1b13729
Lukas Masuch <Lukas.Masuch@gmail.com>
2025-03-06 16:00:40 +0100

    Add issue

---
+++ issues/gh-10310/app.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+st.sidebar.image("https://picsum.photos/1024/1024", caption="Image", use_container_width=True)
+
+
+st.sidebar.markdown("a b c d e f g h i j k l m n o p q r s t u v w x y z a b c d e f g h i j k l m n o p q r s t u v w x y z")
+st.sidebar.markdown("Lorem ipsum dolor sit amet, consectetur adipiscing elit.")
+st.sidebar.markdown("a b c d e f g h i j k l m n o p q r s t u v w x y z")
+st.sidebar.markdown("Lorem ipsum dolor sit amet, consectetur adipiscing elit.")
+
+st.sidebar.markdown("a b c d e f g h i j k l m n o p q r s t u v w x y z a b c d e f g h i j k l m n o p q r s t u v w x y z")
+st.sidebar.markdown("Lorem ipsum dolor sit amet, consectetur adipiscing elit.")
