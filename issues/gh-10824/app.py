dea6766ab023d6ef320aefde28c0df61e50c4e8a
Lukas Masuch <Lukas.Masuch@gmail.com>
2025-03-18 20:00:30 +0100

    Add issue

---
+++ issues/gh-10824/app.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+long_string = "askldfjlweklrjweifjlsdfliwjlierjilsildfjlslfijwilerijlesijlrlijsdflijslijerilweilrilsdfilsilfijlsdf"
+
+st.code(long_string, wrap_lines=True)
