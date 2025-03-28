c268f92a7c3f2aaa2bf29b46e0fa275a0f98c98b
Lukas Masuch <Lukas.Masuch@gmail.com>
2025-03-28 18:56:51 +0100

    Update

--- issues/gh-10937/app.py
+++ issues/gh-10937/app.py
@@ -1,4 +1,6 @@
+import numpy as np
 import pandas as pd
+
 import streamlit as st
 
 is_transposed = st.checkbox("Transpose", value=False)
@@ -13,3 +15,17 @@ df = pd.DataFrame(
 )
 
 st.dataframe(df.T if is_transposed else df)
+st.dataframe(df.T)
+
+with st.form("Generate random dataframe"):
+    num_rows = st.number_input("Number of rows", value=6, min_value=1, max_value=100)
+    num_cols = st.number_input("Number of columns", value=5, min_value=1, max_value=100)
+    prefix = st.text_input("Prefix", value="col_")
+    st.form_submit_button("Generate")
+
+df = pd.DataFrame(
+    np.random.randn(num_rows, num_cols),
+    columns=(prefix + str(i) for i in range(num_cols)),
+)
+
+st.dataframe(df)
