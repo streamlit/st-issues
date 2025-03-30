e03fcef411c2ac1d4bbd3ae9f50e15c8a9550ea2
Lukas Masuch <Lukas.Masuch@gmail.com>
2025-03-30 15:37:58 +0200

    Add issue

---
+++ issues/gh-2910/app.py
@@ -0,0 +1,17 @@
+import altair as alt
+from vega_datasets import data
+
+states = alt.topo_feature(data.us_10m.url, 'states')
+pop = data.population_engineers_hurricanes()
+
+alt.Chart(states).mark_geoshape().encode(
+	color='population:Q'
+).transform_lookup(
+	lookup='id',
+	from_=alt.LookupData(pop, 'id', list(pop.columns))
+).properties(
+	width=500,
+	height=300
+).project(
+	type='albersUsa'
+)
