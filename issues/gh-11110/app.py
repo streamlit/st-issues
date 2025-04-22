b1c56bfe4ebf9ea45ad2e01b79fda76903397bb9
Lukas Masuch <Lukas.Masuch@gmail.com>
2025-04-22 14:56:24 +0200

    Add issue

---
+++ issues/gh-11110/app.py
@@ -0,0 +1,21 @@
+import streamlit as st
+import pandas as pd
+
+def enquadramento(val):
+    if val < 0:
+        return "❌"
+    if val > 0:
+        return "✅"
+    else:
+        return ""
+
+df = pd.DataFrame({
+    'Name': ['John', 'Mary', 'Petter', 'Anna', 'Tom', 'Jerry'],
+    'Total': [-1456.83, 98619.97, -1456.83, 98619.97, -1456.83, 98619.97]
+})
+
+st.dataframe(
+    df.style.format(precision=2, thousands=".", decimal=",")
+        .format(enquadramento, subset=["Total"]),
+        column_config={"Total": {"label": "Enqdnt.", "alignment": "center"}},
+)
