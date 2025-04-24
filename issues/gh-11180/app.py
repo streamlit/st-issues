9f6b9524e6e33b8dc3188243f63cf48c1a957406
Ken McGrady <ken.mcgrady@gmail.com>
2025-04-24 16:23:55 -0700

    Create app.py

---
+++ issues/gh-11180/app.py
@@ -0,0 +1,18 @@
+import streamlit as st
+import pandas as pd
+
+df2 = pd.DataFrame([
+    {"ID": 1, "NAME": "Guillaume"}, 
+    {"ID": 2, "NAME": "Tim"}, 
+    {"ID": 3, "NAME": "me"},
+    {"ID": 4, "NAME": "aname2"},
+    {"ID": 5, "NAME": "aname"}
+])
+df2["NAME"] = df2["NAME"].astype("string")
+
+
+with st.form(key="edit_table2"):
+    new_df2 = st.data_editor(df2, num_rows="dynamic", key="new_df2")
+    submitted = st.form_submit_button("Submit2")
+
+st.write(new_df2)
