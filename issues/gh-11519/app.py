6c6b691cee8c978805cd0982838207e8c5123e71
Lukas Masuch <Lukas.Masuch@gmail.com>
2025-06-04 19:31:05 +0200

    Update app.py

--- issues/gh-11519/app.py
+++ issues/gh-11519/app.py
@@ -2,13 +2,13 @@ import streamlit as st
 import pandas as pd
 
 df = pd.DataFrame({"min_diameter": [4.51]})
-st.data_editor(
+result_df = st.data_editor(
     df,
     column_config={
         "min_diameter": st.column_config.NumberColumn(
             "MIN DM",
             min_value=0.00,
-            step=0.01,
+            # step=0.01,
             format="%.2f",
             width="small",
         )
@@ -16,3 +16,4 @@ st.data_editor(
     num_rows="dynamic",
     use_container_width=True,
 )
+st.dataframe(result_df)
