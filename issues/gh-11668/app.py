bdc5021a02b51d7145d0f32701068b804a27241e
Lukas Masuch <Lukas.Masuch@gmail.com>
2025-06-17 13:08:24 +0200

    Update app.py

--- issues/gh-11668/app.py
+++ issues/gh-11668/app.py
@@ -9,6 +9,12 @@ with st.chat_message("assistant"):
 
 with st.chat_message("assistant"):
     st.markdown(
+        "<div>\n\nSomething1 [source](https://google.com)\n\nSomething2 [source](https://google.com)</div>",
+        unsafe_allow_html=True
+    )
+
+with st.chat_message("assistant"):
+    st.markdown(
         "Something1 [source](https://google.com)\n\nSomething2 [source](https://google.com)",
         unsafe_allow_html=True
     )
