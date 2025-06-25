cfa01258702e796685b41ca565e086922b0e7d18
lukasmasuch <lukas.masuch@gmail.com>
2025-06-25 13:38:58 +0200

    Update

--- issues/gh-11761/app.py
+++ issues/gh-11761/app.py
@@ -1,11 +1,8 @@
-from pathlib import Path
-
 import altair as alt
 import pandas as pd
 import streamlit as st
 
-SCRIPT_DIR = Path(__file__).parent
-df = pd.read_parquet(SCRIPT_DIR / "1167-1.pq")
+df = pd.read_parquet("issues" / "gh-1167" / "1167-1.pq")
 
 actual = (
     alt.Chart(df)
