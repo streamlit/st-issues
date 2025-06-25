60a265b472c1a163abc9c70a6fb4385aa7dcdec9
lukasmasuch <lukas.masuch@gmail.com>
2025-06-25 13:36:37 +0200

    Add issue

---
+++ issues/gh-11761/app.py
@@ -0,0 +1,28 @@
+import altair as alt
+import pandas as pd
+import streamlit as st
+
+df = pd.read_parquet("1167-1.pq")
+
+actual = (
+    alt.Chart(df)
+    .mark_bar()
+    .encode(
+        x="yearmonth(date):T",
+        y="energy_consumption:Q",
+        tooltip=["yearmonth(date):T", "energy_consumption:Q"],
+        fill="split:N",
+    )
+)
+estimate = (
+    alt.Chart(df)
+    .mark_line(color="red", point=alt.OverlayMarkDef(filled=True, fill="red"))
+    .encode(
+        x="yearmonth(date):T",
+        y="baseline:Q",
+        tooltip=["yearmonth(date):T", "baseline:Q"],
+    )
+)
+
+chart = actual + estimate
+st.altair_chart(chart)
