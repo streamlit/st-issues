f9e2d3b4239e4df85b84bb6d22aab0515a7d6325
Lukas Masuch <Lukas.Masuch@gmail.com>
2025-07-04 18:22:23 +0200

    Add issue

---
+++ issues/gh-11845/app.py
@@ -0,0 +1,18 @@
+import streamlit as st
+
+option_map = {
+    0: ":flag-gb:",
+    1: ":flag-ie:",
+    2: ":flag-de:",
+    3: ":flag-at:",
+}
+selection = st.pills(
+    "Tool",
+    options=option_map.keys(),
+    format_func=lambda option: option_map[option],
+    selection_mode="single",
+)
+st.write(
+    "Your selected option: "
+    f"{None if selection is None else option_map[selection]}"
+)
