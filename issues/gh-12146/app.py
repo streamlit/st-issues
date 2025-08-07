630be82e30c21ffc35948970915499c68ab75e3c
Lukas Masuch <Lukas.Masuch@gmail.com>
2025-08-07 15:12:26 +0200

    Add issue

---
+++ issues/gh-12146/app.py
@@ -0,0 +1,15 @@
+import streamlit as st
+
+st.session_state["disable"] = st.session_state.get("disable", False)
+st.session_state["counter"] = st.session_state.get("counter", 0) + 1
+
+st.info(f"Script rerun {st.session_state["counter"]} times")
+
+files = st.file_uploader(label = "", disabled = st.session_state["disable"])
+
+st.info(f"File uploaded: {files is not None}")
+
+button = st.button("Enable" if st.session_state["disable"] else "Disable")
+if button:
+    st.session_state["disable"] = not st.session_state["disable"]
+    st.rerun()
