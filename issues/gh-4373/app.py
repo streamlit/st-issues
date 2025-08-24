bcfcc4d915376f553bf1090b6c422d4789c7ca3d
Lukas Masuch <Lukas.Masuch@gmail.com>
2025-08-24 17:48:38 +0200

    Add issue

---
+++ issues/gh-4373/app.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.markdown("""
+        |  Rank | Change | App | Views |
+        |------:|-------:|-----|------:|
+        | 1 | foo  |  qux | 989 |
+| 2 | bar | baz | 912 |
+""")
