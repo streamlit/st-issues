edf9a4c266df7bdc2dd6e09a4673f7eb04cb893d
Lukas Masuch <Lukas.Masuch@gmail.com>
2025-08-26 15:22:02 +0200

    Update

--- issues/gh-12326/app.py
+++ issues/gh-12326/app.py
@@ -3,4 +3,4 @@ import streamlit as st
 st.logo("https://upload.wikimedia.org/wikipedia/commons/2/20/Artist_Growth_Wide_Logo.png")
 
 with st.sidebar:
-    st.write("Hello")
+    st.write("Hello World")
