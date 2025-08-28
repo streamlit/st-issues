2c02f4a4051a84ede74083fcbf245c2f45dd0a88
Lukas Masuch <Lukas.Masuch@gmail.com>
2025-08-28 11:39:28 +0200

    Add image file upload

--- issues/gh-12355/app.py
+++ issues/gh-12355/app.py
@@ -5,3 +5,9 @@ pdf_file = st.file_uploader("Upload a PDF file", type="pdf")
 if pdf_file:
     st.write(f"PDF uploaded: {pdf_file.name}")
     st.pdf(pdf_file)
+
+
+image_file = st.file_uploader("Upload a image file", type="png")
+if image_file:
+    st.write(f"Image uploaded: {image_file.name}")
+    st.image(image_file)
