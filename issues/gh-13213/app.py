e258e55aec50fa36cfd38ce280db0784cff2c202
Lukas Masuch <Lukas.Masuch@gmail.com>
2025-12-03 22:51:34 +0100

    Add issue

---
+++ issues/gh-13213/app.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+st.write("""
+<style>
+    h1 {
+        padding-left: 50px;
+        background: lime;
+    }
+</style>
+""", unsafe_allow_html = True)
+
+st.markdown('# Foo bar')
