dc3ab7809273fc01a7e020a070a868f5e9d8c961
Lukas Masuch <Lukas.Masuch@gmail.com>
2026-02-14 11:22:28 +0100

    Add Streamlit app to display styled DataFrame

---
+++ issues/gh-6706/app.py
@@ -0,0 +1,18 @@
+import streamlit as st
+import pandas as pd
+
+df = pd.DataFrame({
+    'A':[1,2,3,4],
+    'B':[1111,2,33,44444],
+    'C':[1,2,3,4]
+})
+
+styled_df = df.style.set_properties(**{
+    "background-color": "white", 
+    "color": "black", 
+    "border-color": "black", 
+    'text-align': 'center'
+})
+
+st.dataframe(styled_df)
+st.table(styled_df)
