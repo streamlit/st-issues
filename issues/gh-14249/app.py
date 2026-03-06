fde76e20f609f70aa2f9d818cb20980ba2d206ff
Lukas Masuch <Lukas.Masuch@gmail.com>
2026-03-06 18:49:05 +0100

    Update app.py

--- issues/gh-14249/app.py
+++ issues/gh-14249/app.py
@@ -13,6 +13,8 @@ section = st.segmented_control("Section", ["S1", "S2"])
 if section == "S1":
     st.markdown("- S1.1")
     st.markdown("- S1.2")
+    st.markdown("- S1.3")
+    st.markdown("- S1.4")
 elif section == "S2":
     (tab,) = st.tabs(["S2.Tab"])
     foo()
