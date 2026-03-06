e748673f9e9b752ec2465ce6fe5f31943ca01587
Lukas Masuch <103002573+sfc-gh-lmasuch@users.noreply.github.com>
2026-03-06 18:03:28 +0100

    Add issue

---
+++ issues/gh-14249/app.py
@@ -0,0 +1,19 @@
+import time
+
+import streamlit as st
+
+
+@st.cache_data
+def foo():
+    time.sleep(2)
+
+
+section = st.segmented_control("Section", ["S1", "S2"])
+
+if section == "S1":
+    st.markdown("- S1.1")
+    st.markdown("- S1.2")
+elif section == "S2":
+    (tab,) = st.tabs(["S2.Tab"])
+    foo()
+    tab.markdown("S2 Lorem")
